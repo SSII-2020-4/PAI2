c4eaeb26b95c09f4464df0cdff81c3ebb8fe4b7e
Albert0led0 <a.toledo96@gmail.com>
2020-03-19 18:12:29 +0100

    feature (nonce): nonce uniqueness

--- src/core/server_utils.py
+++ src/core/server_utils.py
@@ -15,7 +15,7 @@ class ServerUtils():
     Class for server
     """
 
-    def calculate_MAC(key : bytes, message : bytes, nonce : bytes, algorithm=hashlib.sha256):
+    def calculate_MAC(self, key : bytes, message : bytes, nonce : bytes, algorithm=hashlib.sha256):
         """
         Calcula el MAC de un mensaje y el nonce, pasando la clave como parámetros. Estos 3 campos deben ser en bytes.
 
@@ -29,7 +29,7 @@ class ServerUtils():
         digest_maker.hexdigest()
         return digest_maker.hexdigest()
 
-    def get_transference_rate(is_integrity_violated : bool, message : str):
+    def get_transference_rate(self, is_integrity_violated : bool, message : str):
         """
         Devuelve la tasa de transferencia mensajes_enviados_integros / mensajes_totales. Además, acutaliza el archivo de logs.
 
@@ -47,10 +47,9 @@ class ServerUtils():
                 loaded_rate["success"] = loaded_rate["success"] + 1            
             else:
                 with open(os.path.join(os.getcwd(), "files", "logs.log"), "a+", encoding="utf-8") as file:
-                    file.write("[" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "] - Se ha violado la integridad del mensaje: " + message "\n")
-
+                    file.write("[" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "] - Se ha violado la integridad del mensaje: " + message +"\n")
             rate = loaded_rate["success"] / loaded_rate["total"] * 100
-        except ZeroDivisionError as e:
+        except ZeroDivisionError:
             rate = 0
         finally:
             with open(os.path.join(os.getcwd(), "files", ".transference_rate_backup"), "w") as file:
@@ -61,8 +60,24 @@ class ServerUtils():
     
     def gen_nonce(self, length=32):
         """ Generates a random string in hexadecimal with 32 random bytes by default """
-        if(length<32):
-            res= 'Invalid nonce length', 400
+        if(length<1):
+            res= {"message":'Invalid nonce length'}, 400
         else:
-            res= secrets.token_hex(floor(length)), 200
+            res= {"nonce":secrets.token_hex(floor(length))}, 200
+            if(not os.path.isfile("server-nonces.txt")):
+                f = open("server-nonces.txt","w")
+            f = open("server-nonces.txt","r")
+            linea = f.readline()
+            aux = True
+            while linea != "":
+                if(linea == res[0]["nonce"]+"\n"):
+                    aux = False
+                    break
+                linea = f.readline()
+            if(aux):
+                f = open("server-nonces.txt","a")
+                f.write(res[0]["nonce"]+"\n")
+            else:
+                res = {"message":'Used nonce'}, 401
         return res
+    
