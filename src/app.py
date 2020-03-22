eeb6b13e4611fb348aae231274c6ea311c64ece6
Fernando <fheraa@gmail.com>
2020-03-22 19:08:54 +0100

    fix_some_issues

--- src/app.py
+++ src/app.py
@@ -9,28 +9,22 @@ from apis import api
 load_dotenv(".env")
 
 # Comprobar que las variables de entornos necesias existen
-# strMensajeVaribleError = "ERROR , NO SE HA DEFINIDO LA VARIABLE : "
+strMensajeVaribleError = "ERROR , NO SE HA DEFINIDO LA VARIABLE : "
 
 
-# def check_enviroment():
-#     b_check_environment = True
+def check_enviroment():
+    b_check_environment = True
 
-#     if ("TOKENS" not in os.environ):
-#         print(strMensajeVaribleError + "TOKENS")
-#         b_check_environment = False
-#     if ("DAILY_ANALYSIS" not in os.environ):
-#         print(strMensajeVaribleError + "DAILY_ANALYSIS")
-#         b_check_environment = False
-#     if ("CHECK_INTEGRITY" not in os.environ):
-#         print(strMensajeVaribleError + "CHECK_INTEGRITY")
-#         b_check_environment = False
-#     return b_check_environment
+    if ("TOKENS" not in os.environ):
+        print(strMensajeVaribleError + "TOKENS")
+        b_check_environment = False
+    return b_check_environment
 
 
-# b_entorno = check_enviroment()
+b_entorno = check_enviroment()
 
-# if b_entorno:
-app = Flask(__name__)
-api.init_app(app)
-if __name__ == "__main__":
-    app.run(debug=True)
+if b_entorno:
+    app = Flask(__name__)
+    api.init_app(app)
+    if __name__ == "__main__":
+        app.run(debug=True)
